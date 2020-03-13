61183e4d952d6243291aef34627203ddb1fa4919
kchan2000 <katiechan@Katies-MacBook-Pro.local>
2020-03-12 17:49:04 -0700

    week 6

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+$document.ready(function(){
+
+var allPs = $("p");
+console.log(allPs);
+
+var classp = $".paragraph1");
+console.log(classp);
+
+var idp= ("ptag");
+console.log(idp);
+
+allPs.html("hi this is my new text")
+
+
+
+});
